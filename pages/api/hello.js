c0c3049e92aadc3ac0f22aed490eed30d19a15c7
Furkan Kalaycıoğlu <Furkan0Kalaycioglu@gmail.com>
2022-07-17 03:15:53 +0300

    test gitignore firebaseconfig

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { firebaseConfig } from "./firebase-config";
 
+const test = firebaseConfig.apiKey.toString();
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: test + " from hello.js" });
 }
