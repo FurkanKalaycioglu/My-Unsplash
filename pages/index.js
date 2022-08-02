543b97e45527c8141a60aed362fff641914f4207
Furkan Kalaycıoğlu <Furkan0Kalaycioglu@gmail.com>
2022-08-02 16:58:01 +0300

    Hover effect for images

--- pages/index.js
+++ pages/index.js
@@ -111,11 +111,11 @@ export default function Home() {
             }}
             hasMore={hasMore}
             loader={<h4>Loading...</h4>}
-            endMessage={
-              <p>
-                <b>Yay! You have seen it all</b>
-              </p>
-            }
+            // endMessage={
+            //   <div className="">
+            //     <h1>Sadly, No More Pictures :(</h1>
+            //   </div>
+            // }
           >
             {/* find something else other than margin for this problem*/}
 
@@ -128,15 +128,35 @@ export default function Home() {
                     : " w-[385px] h-[300px]")
                 }
               >
+                <div className="absolute inset-0 z-10 opacity-0  0 hover:opacity-100 duration-300 ">
+                  <div className="bg-black bg-opacity-0 p-4 w-full h-full hover:bg-opacity-50 transition-all duration-500">
+                    <button
+                      type="button"
+                      class="absolute top-4 right-4 px-4 py-1 text-[#EB5757] hover:text-white border border-[#EB5757] hover:bg-[#EB5757] font-medium rounded-3xl text-sm text-center "
+                    >
+                      Delete
+                    </button>
+
+                    <h2 className="text-white absolute inset-x-0 bottom-0 p-6 text-3xl font-bold">
+                      {item.label}
+                    </h2>
+                  </div>
+                </div>
+
                 <Image
                   src={item.url}
                   layout="fill"
-                  className="rounded-xl object-cover text-white"
+                  className="rounded-xl object-cover text-white absolute inset-0 z-0"
                   title={item.label}
                 />
               </div>
             ))}
           </InfiniteScroll>
+          {hasMore ? null : (
+            <div className="flex justify-center text-3xl font-extrabold m-16">
+              <h1>Sadly, No More Pictures :(</h1>
+            </div>
+          )}
         </div>
       </main>
     </div>
